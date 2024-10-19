bce25e482b6ce73447dabd04ee36f80438e7ad77
dals5 <aszx040@naver.com>
2024-10-20 07:00:37 +0900

    .

---
+++ KMH_ch02/src/ch02_4_02/KeyCodeExample.java
@@ -0,0 +1,17 @@
+package ch02_4_02;
+
+public class KeyCodeExample {
+
+	public static void main(String[] args) throws Exception { 
+		int keyCode;
+		
+		keyCode = System.in.read();
+		System.out.println("keyCode : " + keyCode); 
+		
+		keyCode = System.in.read();
+		System.out.println("keyCode : " + keyCode); 
+		
+		keyCode = System.in.read();
+		System.out.println("keyCode : " + keyCode); 
+	} 
+} 
