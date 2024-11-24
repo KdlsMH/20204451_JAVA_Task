3b766e3ec3d9f571f5e5722b330a6553e52165b6
dals5 <aszx040@naver.com>
2024-11-24 20:49:47 +0900

    .

---
+++ KMH_ch09/src/ch09_2_04/AnonymousExample.java
@@ -0,0 +1,10 @@
+package ch09_2_04;
+
+public class AnonymousExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Anonymous anony = new Anonymous();
+		anony.method(0, 0);
+	}
+}
---
+++ KMH_ch09/src/ch09_2_04/Calculatable.java
@@ -0,0 +1,5 @@
+package ch09_2_04;
+
+public interface Calculatable {
+	public int sum();
+}
