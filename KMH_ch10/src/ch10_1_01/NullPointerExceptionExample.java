9758b3ed69599f4d492a1635cfc0dc24bd9d2052
dals5 <aszx040@naver.com>
2024-11-28 00:40:33 +0900

    .

---
+++ KMH_ch10/src/ch10_1_01/NullPointerExceptionExample.java
@@ -0,0 +1,10 @@
+package ch10_1_01;
+
+public class NullPointerExceptionExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String data = null;
+		System.out.println(data.toString());
+	}
+}
